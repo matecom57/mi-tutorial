e5678eecf4b185ac3048714f4290d542c31f5d79
santosg572 <santosg572@gmail.com>
2025-04-25 06:58:34 -0600

    Test comité

--- docs/source/EcuDif_Relay/pTeor.R
+++ docs/source/EcuDif_Relay/pTeor.R
@@ -7,22 +7,25 @@
 tao = 1.5
 niter = 14
 
-np = 100
-t = seq(-1*tao, 0, length.out = np)
+GenSolTeorica <- function(tao=0, niter=1){
+  np = 100
+  t = seq(-1*tao, 0, length.out = np)
 
-x = rep(1,np)
-xx = x
-tt = t
+  x = rep(1,np)
+  xx = x
+  tt = t
 
-for (n in 1:niter){
-  res = 1
-  t = t + tao
-  for (k in 1:n){
+  for (n in 1:niter){
+    res = 1
+    t = t + tao
+    for (k in 1:n){
     res = res + (-1)^k *(t - (k-1)*tao)^k / factorial(k)
   }
   xx = c(xx, res)
   tt = c(tt,t)
+  res = list(xx, tt)
 }  
 
-plot(tt, xx, type='l')
+r <- GenSolTeorica()
+plot(r[[1]], r[[2]], type='l')
 
