8dca0d34095597668ecff920d8a89a45cd8e9b92
santosg572 <santosg572@gmail.com>
2025-04-28 15:03:23 -0600

    Test comité

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,11 +29,11 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
-
 html_css_files = [
     'css/css-style.css',
 ]
+
+html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
