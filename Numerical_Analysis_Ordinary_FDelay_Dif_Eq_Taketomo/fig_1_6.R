4325e4bf632bdc5a999b564dad0fa5ffbe8d8029
santosg572 <santosg572@gmail.com>
2025-05-23 12:53:45 -0600

    Test comité

--- Numerical_Analysis_Ordinary_FDelay_Dif_Eq_Taketomo/fig_1_6.R
+++ Numerical_Analysis_Ordinary_FDelay_Dif_Eq_Taketomo/fig_1_6.R
@@ -1,69 +1,35 @@
-Resuelve <- function(vali=c(0,0)){
-   x1 = vali
+Integral <- function(ff=0, tao=0){
+  del = .001
+  t = seq(0,tao, del)
+  np = length(t)
+  ss = dim(ff)
+  print(ss)
+  x1 = ff[,ss[2]]
 
-   del = .001
-   epsi=1
-
-   t = seq(0,40,del)
-   np = length(t)
-   x = matrix(rep(0, 2*np), nrow=2)
-
-   x[,1] = x1
-   for (i in 2:np){
-      x2 = x1 + del* c(epsi*x1[1] * (1 - x1[1]^2/3) - x1[2], x1[1])
-      x[,i] = x2
-      x1 = x2
-   }
-   res = list(t=t, v=x[1,])
-}
-
-Integral <- function(del, f){
-  np = 100
-  epsi = 1
   x = matrix(rep(0, 2*np), nrow=2)
 
-  x[,1] = f[,np]
-  x1 = x[,1]
+  x[,1] = x1
+  e=1
 
   for (i in 2:np){
-    x2 = x1 - del * c(epsi * f[1,i-1] * (1 - f[1,i-1] * f[1,i-1]/3) - f[2,np], f[1,np])  
+    f = ff[,i-1]
+    x2 = x1 + del * c(e*f[1]*(1-f[1]*f[1]/3)-x1[2], x1[1])
     x[,i] = x2
     x1 = x2
   }
-  ret = x
+  res = x
 }
 
-np = 100
+tao = .2
+del = .001
+tt = seq(0,tao,del)
+np = length(t)
+ff = matrix(rep(c(2,0),np), nrow=2)
 
-tao=.02
-
-t = seq(-1*tao,0,length.out = np)
-
-del = t[2] - t[1]
-
-f = matrix(rep(c(2,0),2*np), nrow=2)
-ff = f
-tt = t
-
-niter=10
-
-for (i in 1:niter){
-  t = t+tao
-  x = Integral(del, f)
-  ff = cbind(ff, x)
-  tt = cbind(tt, t)
-  f = x
+for (i in 1:1000){
+  ff = Integral(ff, tao) 
 }
 
-r = Resuelve(c(2,0))
-
-#plot(r$t, r$v, type='l')
-
-print(length(tt))
-print(dim(ff))
-
-plot(tt, ff[1,1:1100], type='l', col='red')
-
-
+plot(ff[1,], type='l')
 
 
